436532dd916ecd7adc73b9f1d7beef1ed4cd3259
Chris Beer <chris@cbeer.info>
2015-05-29 12:20:59 -0700

    Do a lazy job populating type-specific fields for steps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,23 @@
 //= require jquery_nested_form
 //= require angular
 //= require_tree .
+
+$(document).on("ready page:load", function() {
+  $('.step select').on('change', function() {
+    var options = $(this).closest('.step').find('.options');
+    var current_options = step_type_options[$(this).val()];
+    options.find('.form-group').filter(function() {
+      return current_options.indexOf($(this).find('input').data('option')) == -1;
+    }).remove();
+
+    var filtered_options = $.grep(current_options, function(n) {
+      return options.has('input[data-option="' + n + '"]').length === 0;
+    });
+
+    var template = options.find('.form-group')[0].outerHTML;
+
+    $.each(filtered_options, function(i) {
+      options.append(template.replace(/field/ig, filtered_options[i]));
+    });
+  });
+});
